ac09b25926286abaf5bed9e9f1c15ab867ae8505
mattjcamp <mjcampbell74@gmail.com>
2017-03-02 08:32:34 -0500

    Made show_columns match on schema and match on information schema table instead of attempting to load an entire table and then retrieve the names

--- R/show_columns.R
+++ R/show_columns.R
@@ -4,13 +4,48 @@
 #' Quick way to see what columns are in a table
 #' @description Shows the columns in the table you specify
 #' @param conn RODBC, RJDBC or other supported data connection
-#' @param table the table that you want to inspect
+#' @param schema the schema or database that the tables are located in
+#' @param tables the table that you want to inspect
 #' @export
 #' 
-show_columns <- function(conn, table) {
+#' 
+show_columns <- function(conn, schema, tables) {
   
-  sprintf("select top 1 * from %s", table) %>% 
-    pull_data(conn) %>% 
-    names()
+    if (class(conn) %in% "JDBCConnection")
+      sql <- "
+
+select  table_name, table_schema, column_name, data_type, 
+        character_maximum_length as col_length, ordinal_position as column_order
+from information_schema.columns
+where table_name in INSPECT_TABLE and table_schema in ('INSPECT_SCHEMA')
+order by table_schema, table_name, ordinal_position"
+
+    if (class(conn) %in% "RODBC")
+      sql <- "
+
+select  table_name, table_schema, column_name, data_type, 
+        character_maximum_length as col_length, ordinal_position as column_order
+from INSPECT_SCHEMA.information_schema.columns
+where table_name in INSPECT_TABLE and table_schema in ('dbo')
+order by table_schema, table_name, ordinal_position"
+    
+  d <- 
+    sql %>% str_replace("INSPECT_SCHEMA", schema) %>%
+    str_replace("INSPECT_TABLE", code_vector_to_csv_list(tables,
+                                                         add.quotes = TRUE,
+                                                         enclose.in.parenthesis = TRUE)) %>% 
+  pull_data(conn) %>% 
+  mutate(table_name = str_to_lower(table_name),
+         column_name = str_to_lower(column_name),
+         col_length = as.numeric(col_length)) %>% 
+  mutate(data_type = ifelse(data_type %in% c("character varying"),
+                            "varchar", data_type),
+         data_type = ifelse(data_type %in% c("int", "tinyint", "smallint"),
+                            "numeric", data_type),
+         data_type = ifelse(data_type %in% c("datetime2", "datetime", "timestamp without time zone"),
+                            "date", data_type))
+
   
+  d
+
 }
