ad9e47956b5e1cb4282d5c5520a59049664dfa9a
mattjcamp <mjcampbell74@gmail.com>
2017-03-03 14:12:20 -0500

    Adjusted show_columns to remove dbo when looking at information schema

--- R/show_columns.R
+++ R/show_columns.R
@@ -7,37 +7,39 @@
 #' @param schema the schema or database that the tables are located in
 #' @param tables the table that you want to inspect
 #' @export
-#' 
-#' 
+#'
+#'
 show_columns <- function(conn, schema, tables) {
-  
+
     if (class(conn) %in% "JDBCConnection")
       sql <- "
 
-select  table_name, table_schema, column_name, data_type, 
+select  table_name, table_schema, column_name, data_type,
         character_maximum_length as col_length, ordinal_position as column_order
 from information_schema.columns
 where table_name in INSPECT_TABLE and table_schema in ('INSPECT_SCHEMA')
 order by table_schema, table_name, ordinal_position"
 
-    if (class(conn) %in% "RODBC")
+    if (class(conn) %in% "RODBC") {
+      schema <- stringr::str_replace(schema, ".dbo", "")
       sql <- "
 
-select  table_name, table_schema, column_name, data_type, 
+select  table_name, table_schema, column_name, data_type,
         character_maximum_length as col_length, ordinal_position as column_order
 from INSPECT_SCHEMA.information_schema.columns
 where table_name in INSPECT_TABLE and table_schema in ('dbo')
 order by table_schema, table_name, ordinal_position"
-    
-  d <- 
+    }
+
+  d <-
     sql %>% str_replace("INSPECT_SCHEMA", schema) %>%
     str_replace("INSPECT_TABLE", code_vector_to_csv_list(tables,
                                                          add.quotes = TRUE,
-                                                         enclose.in.parenthesis = TRUE)) %>% 
-  pull_data(conn) %>% 
+                                                         enclose.in.parenthesis = TRUE)) %>%
+  pull_data(conn) %>%
   mutate(table_name = str_to_lower(table_name),
          column_name = str_to_lower(column_name),
-         col_length = as.numeric(col_length)) %>% 
+         col_length = as.numeric(col_length)) %>%
   mutate(data_type = ifelse(data_type %in% c("character varying"),
                             "varchar", data_type),
          data_type = ifelse(data_type %in% c("int", "tinyint", "smallint"),
@@ -45,7 +47,7 @@ order by table_schema, table_name, ordinal_position"
          data_type = ifelse(data_type %in% c("datetime2", "datetime", "timestamp without time zone"),
                             "date", data_type))
 
-  
+
   d
 
 }
